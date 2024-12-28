43142c78bfe1d1461bf8ad89b3afd17729bc0220
ovo <ovo@ovodeMacBook-Air.local>
2024-12-28 16:24:52 +0800

    vue demo

---
+++ src/constants/storage.ts
@@ -0,0 +1,3 @@
+export const STORAGE_KEYS = {
+    TEXT_DATA: 'aaData'
+} as const 
---
+++ src/popup/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <textarea rows="2" cols="5" v-model="myText" />
+    </div>
+    <button @click="saveData">Save</button>
+    <button @click="showData">showData</button>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { STORAGE_KEYS } from '../constants/storage'
+const myText = ref('')
+
+onMounted(() => {
+    myText.value = localStorage.getItem(STORAGE_KEYS.TEXT_DATA)
+})
+
+const saveData = () => {
+    localStorage.setItem(STORAGE_KEYS.TEXT_DATA, myText.value)
+}
+
+const showData = () => {
+    alert(localStorage.getItem(STORAGE_KEYS.TEXT_DATA))
+}
+</script>
+
+<style scoped></style>
